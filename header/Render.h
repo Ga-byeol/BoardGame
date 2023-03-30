aae704176ae7f9c55fcf531f5829ebff65afa789
Gabyeol <116336965+Ga-byeol@users.noreply.github.com>
2023-03-30 17:34:32 +0900

    Update Render.h

--- header/Render.h
+++ header/Render.h
@@ -9,12 +9,12 @@ protected:
 	int nSize;
 	bool bwide;
 	string line[11] = {
-		"式","弛",
-		"忙","成","忖",
-		"戍","托","扣",
-		"戌","扛","戎"
+		"─","│",
+		"┌","┬","┐",
+		"├","┼","┤",
+		"└","┴","┘"
 	};		
-	string stone[2] = { "∞","≒" };	
+	string stone[2] = { "○","●" };	
 	void printUITop();	
 	string getLine(string, int, int);
 	void printUIRight(int);
